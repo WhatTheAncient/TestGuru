cfc08aa8f2367e304c86856f0ead5776ee85e422
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-12-17 14:46:43 +0300

    hotfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :results, dependent: :destroy
   has_many :created_tests, inverse_of: :author, class_name: 'Test', foreign_key: 'author_id'
+  has_many :results, dependent: :destroy
   has_many :tests, through: :results
 
   def tests_history(level) #Returns started and finished tests
