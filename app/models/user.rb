ac4f11679299b2b4c1cee0b03fea394f9f8c504a
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-12-19 11:18:40 +0300

    hotfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :created_tests, inverse_of: :author, class_name: 'Test', foreign_key: 'author_id'
+  has_many :created_tests, inverse_of: :author, class_name: 'Test',
+                           foreign_key: 'author_id', dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
 
