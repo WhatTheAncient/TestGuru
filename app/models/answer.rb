b9de7338e9eef2f9587e087b59c221cd554dfe15
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-12-20 14:04:37 +0300

    add to on: :create option on count validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,13 +3,13 @@ class Answer < ApplicationRecord
 
   validates :title, presence: true
 
-  validate :validate_count
+  validate :validate_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_count
-    errors.add(:answers_count) unless (1..4).include? question.answers.count + 1
+    errors.add(:answers_count) unless question.answers.count < 4
   end
 end
