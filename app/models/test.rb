f339ffa5b641111a07b99d9eab20644d4ec6840f
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-12-21 23:54:03 +0300

    add rubocope and do some style changes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', inverse_of: :created_tests
   belongs_to :category
@@ -9,8 +11,10 @@ class Test < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
-  scope :tests_with_category, ->(category_title) { joins(:category).where(category: { title: category_title })
-                                                                  .order(tests: :desc) }
+  scope :tests_with_category, lambda { |category_title|
+                                joins(:category).where(category: { title: category_title })
+                                                .order(tests: :desc)
+                              }
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 4..Float::INFINITY) }
