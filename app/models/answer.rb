63b79dcaccd2076521ca8a12ad092bd0bc5c17bf
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-12-21 15:50:24 +0300

    change unless to if in :validate_count

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -10,6 +10,6 @@ class Answer < ApplicationRecord
   private
 
   def validate_count
-    errors.add(:answers_count) unless question.answers.count < 4
+    errors.add(:answers_count) if question.answers.count >= 4
   end
 end
