ed5ec1ca020a0c0a405d83dc80ebfb4cef363b28
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-12-23 02:12:27 +0300

    add QuestionsController methods
    added #show #inde #create #destroy methods. Also add some callbacks and resque_with_question_not_found method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,44 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index show destroy]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
-    respond_to do |format|
-      format.html { render inline: "<p> <%= Test.find(#{params[:test_id]}).questions.pluck(:title)%> </p>" }
-      format.text { render plain: 'All tests' }
-    end
+    render inline: '<p> <%= @test.questions.pluck(:title) %> </p>'
   end
 
   def show
-
+    render inline: '<p> <%= @question.title %> </p>'
   end
 
   def create
-    q = Question.create!(title: question_params[:question][:title], test_id: question_params[:test_id])
+    @question = Question.create!(title: question_params[:question][:title], test_id: question_params[:test_id])
+
     render plain: "New question is added to #{params[:test_id]} test."
   end
 
   def destroy
-
+    @question.destroy
   end
 
   private
 
   def question_params
-    params.permit({ question: [:title] }, :test_id)
+    params.permit({ question: [:title] }, :test_id, :id)
+  end
+
+  def find_test
+    @test = Test.find(question_params[:test_id])
+  end
+
+  def find_question
+    @question = @test.questions.find(question_params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions
   end
 end
