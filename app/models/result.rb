19d9b5518c5ac30670e1f0cf40bf1d19c19c2d34
WhatTheAncient <whattheanc1ent@yandex.ru>
2022-01-07 12:31:15 +0300

    merge 2 callbacks in before_validation_set_current_question

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,9 +5,8 @@ class Result < ApplicationRecord
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', foreign_key: :current_question_id, optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_question, on: :update
-  
+  before_validation :before_validation_set_current_question
+
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
@@ -38,17 +37,17 @@ class Result < ApplicationRecord
   end
 
   def next_question
-    test.questions
-        .order(:id)
-        .where('id > ?', current_question.id)
-        .first
+    if current_question.nil?
+      test.questions.first if test.present?
+    else
+      test.questions
+          .order(:id)
+          .where('id > ?', current_question.id)
+          .first
+    end
   end
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
-  def before_validation_set_next_question
+  def before_validation_set_current_question
     self.current_question = next_question
   end
 end
