9d15426fa8637b53e49eba1485f519fae4b66fc1
WhatTheAncient <whattheanc1ent@yandex.ru>
2022-01-11 22:39:42 +0300

    delete useless controllers and refactor flash_message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
   end
 
   def flash_message(type)
-    content_tag :p, flash[type], class: "flash #{type.to_s}"
+    content_tag :p, flash[type], class: "flash #{type.to_s}" if flash[type]
   end
 end
