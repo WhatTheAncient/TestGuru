fe98cc58b5878ceae56ac2da301585f25b3e438c
WhatTheAncient <whattheanc1ent@yandex.ru>
2022-01-18 16:52:29 +0300

    add Tests sorting by title and make some changes with stylesheets

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,14 +1,57 @@
 document.addEventListener('turbolinks:load', function() {
     var control = document.querySelector('.sort-by-title')
 
-    console.log(control)
-    control.addEventListener('click', sortRowsByTitle)
+    if (control) { control.addEventListener('click', sortRowsByTitle) }
 })
 
 function sortRowsByTitle() {
     var table = document.querySelector('table')
 
     var rows = table.querySelectorAll('tr')
+    var sortedRows = []
 
-    console.log(rows)
+    for (var i = 1; i < rows.length; i++) {
+        sortedRows.push(rows[i])
+    }
+
+    var arrowUp = document.querySelector('.octicon-arrow-up')
+    var arrowDown = document.querySelector('.octicon-arrow-down')
+
+    if (arrowUp.classList.contains('hide')) {
+        sortedRows.sort(compareRowsAsc)
+        arrowUp.classList.remove('hide')
+        arrowDown.classList.add('hide')
+    } else {
+        sortedRows.sort(compareRowsDesc)
+        arrowDown.classList.remove('hide')
+        arrowUp.classList.add('hide')
+    }
+
+    var sortedTable = document.createElement('table')
+    sortedTable.classList.add('table')
+    sortedTable.appendChild(rows[0])
+
+    for (var i = 0; i < sortedRows.length; i++) {
+        sortedTable.appendChild(sortedRows[i])
+    }
+
+    table.parentNode.replaceChild(sortedTable, table)
+}
+
+function compareRowsAsc(row1, row2) {
+    var testTitle1 = row1.querySelector('td').textContent
+    var testTitle2 = row2.querySelector('td').textContent
+
+    if (testTitle1 < testTitle2) { return -1 }
+    if (testTitle1 > testTitle2) { return 1 }
+    return 0
+}
+
+function compareRowsDesc(row1, row2) {
+    var testTitle1 = row1.querySelector('td').textContent
+    var testTitle2 = row2.querySelector('td').textContent
+
+    if (testTitle1 < testTitle2) { return 1 }
+    if (testTitle1 > testTitle2) { return -1 }
+    return 0
 }
